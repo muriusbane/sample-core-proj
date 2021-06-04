d867424f2d3d84e4778fbccee7153efbfe325bdd
Renellie Alfred Maglasang <technurvuz@gmail.com>
2021-06-04 21:24:01 +0800

    Add string Name

--- sample-core-proj/Models/category.cs
+++ sample-core-proj/Models/category.cs
@@ -7,8 +7,10 @@ using System.Threading.Tasks;
 namespace sample_core_proj.Models
 {
     public class Category
-    {
+    { 
         [Key]
         public int Id { get; set; }
+        public string Name { get; set; }
+
     }
 }
