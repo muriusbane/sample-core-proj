4678e7774dbbf0795de44b7e74390d170731bca9
Renellie Alfred Maglasang <technurvuz@gmail.com>
2021-06-04 21:22:22 +0800

    Use Data Annotations

--- sample-core-proj/Models/category.cs
+++ sample-core-proj/Models/category.cs
@@ -1,5 +1,6 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace sample_core_proj.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
     }
 }
