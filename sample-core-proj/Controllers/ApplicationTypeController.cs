bbfdabfb11f19e3b5a2dadea06927909261bb9ea
Renellie Alfred Maglasang <technurvuz@gmail.com>
2021-06-05 00:20:29 +0800

    Add ApplicationTypeController

---
+++ sample-core-proj/Controllers/ApplicationTypeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using sample_core_proj.Data;
+using sample_core_proj.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sample_core_proj.Controllers
+{
+    public class ApplicationTypeController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ApplicationTypeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<ApplicationType> objList = _db.ApplicationType;
+            return View(objList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ApplicationType obj)
+        { 
+            _db.ApplicationType.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
